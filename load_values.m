aa254e5813603e1e3e287a6be29683c4ba33630c
Wesley Soo-Hoo <wesoohoo@gmail.com>
2022-03-22 20:59:25 -0400

    add matlab files

---
+++ load_values.m
@@ -0,0 +1,16 @@
+% System parameters
+g = 9.8;
+l = 0.4195;
+
+% Motor parameters
+a = 12.5;
+b = 0.00323;
+
+% Theta control loop
+Kp = 4.086;
+Ki = 19.804;
+
+% motor control loop
+Jp = -12.38;
+JiCp = 598;
+Ci = 659;
